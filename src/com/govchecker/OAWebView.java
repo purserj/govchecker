021cc3b54562fc70656da0a6182204ea93349701
James Purser <jamesp@catalsyt-au.net>
2014-10-31 14:26:16 +1100

    Added ability to share links to OpenAustralia website from hansard webview

--- src/com/govchecker/OAWebView.java
+++ src/com/govchecker/OAWebView.java
@@ -3,13 +3,17 @@ package com.govchecker;
 import com.govchecker.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.webkit.WebView;
+import android.widget.ShareActionProvider;
 
 public class OAWebView extends Activity{
 	
 	private WebView wv;
+	private ShareActionProvider mShareActionProvider;
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -20,6 +24,24 @@ public class OAWebView extends Activity{
 		Log.d("fin_url", extras.getString("finurl"));
 		
 		wv.loadUrl(extras.getString("finurl"));
+	}
+	
+	public boolean onCreateOptionsMenu(Menu menu){
+		getMenuInflater().inflate(R.menu.menu_webview, menu);
+		
+		mShareActionProvider = (ShareActionProvider) menu.findItem(R.id.share).getActionProvider();
+		
+		mShareActionProvider.setShareIntent(getDefaultShareIntent());
+		
+		return super.onCreateOptionsMenu(menu);	
+	}
+	
+	private Intent getDefaultShareIntent(){
+		Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.putExtra(Intent.EXTRA_SUBJECT, "SUBJECT");
+		intent.putExtra(Intent.EXTRA_TEXT, wv.getUrl().toString());
+		return intent;
 	}
 
 }
